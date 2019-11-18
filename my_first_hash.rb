25a3aacaa3f5bb1f303fef82dc67ebe179c1cd21
Luana Faria Lane <luanasfff@gmail.com>
2019-11-18 21:52:14 +0000

    Done.

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,9 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+  my_own_hash = {
+            "my_name" => 24,
+            "my_location" => "Atlanta, Ga",
+            "my_job" => "Student"
+  }
   
 end
 
@@ -21,7 +24,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+shipping_manifest ["oil paintings"]
 end
 
 def adding
@@ -29,15 +32,11 @@ def adding
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
-                 }
-  # add 2 muskets to the shipping_manifest hash below
+                 } 
+
+shipping_manifest ["muskets"] = 2
+shipping_manifest["gun powder"] = 4
+  shipping_manifest
 
 
 
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
-
-end
